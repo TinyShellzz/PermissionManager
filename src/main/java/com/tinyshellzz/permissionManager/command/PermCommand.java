dfbeb999cc8d3e84e5733be577f52d8556c7b9e4
TinyShellzz <tinyshellzz@gmail.com>
2024-07-18 09:04:24 +0800

    fix

--- src/main/java/com/tinyshellzz/permissionManager/command/PermCommand.java
+++ src/main/java/com/tinyshellzz/permissionManager/command/PermCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -16,10 +17,18 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PermCommand  implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Bukkit.getConsoleSender().sendMessage("sender: " + sender.getName());
+        Matcher _m = Pattern.compile("^.*CraftRemoteConsoleCommandSender.*$").matcher(sender.toString());
+        if(!(sender instanceof ConsoleCommandSender || _m.find() || sender.isOp() || sender.hasPermission("perm.use"))){
+            sender.sendMessage(ChatColor.RED +  "你没有权限使用该命令");
+            return true;
+        }
         if (args.length == 0) {
             sender.sendMessage(ChatColor.YELLOW + "参数不足");
             return true;
